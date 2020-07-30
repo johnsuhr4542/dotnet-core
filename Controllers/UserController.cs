939f1abefe511e2c961d16351a58196f888b8807
kkim4542 <kkim4542@tolan.kr>
2020-07-30 18:05:30 +0900

    edit some changes

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NLog;
@@ -13,8 +15,15 @@ namespace application.Controllers {
     [HttpPost]
     public async Task<IActionResult> Login(string username, string password) {
       await Task.Run(() => {});
-
+      
       _logger.Info($"username : {username}, password : {password}");
+
+      // here authentication
+
+      var claims = new List<Claim> {
+        new Claim(ClaimTypes.Name, username),
+        new Claim(ClaimTypes.Role, "ROLE_MEMBER")
+      };
 
       return View();
     }
